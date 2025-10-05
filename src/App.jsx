949d7b1bcf1fbd38411c95b2c51ece10e9cf32a0
정화윤 <jeonghwayun0119@gmail.com>
2025-10-05 10:11:35 +0900

    Feat: Update - 투두 수정하기

--- src/App.jsx
+++ src/App.jsx
@@ -40,11 +40,18 @@ function App() {
     setTodos([newTodo, ...todos]);
   }
 
+  const onUpdate = (targetId) => {
+    // todos State의 값들 중에
+    // targetId와 일치하는 id를 갖는 투두 아이템의 isDone 변경
+
+    setTodos(todos.map((todo) => todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo));
+  }
+
   return (
     <div className="App">
       <Header />
       <Editor onCreate={onCreate} />
-      <List todos={todos} />
+      <List todos={todos} onUpdate={onUpdate} />
     </div>
   );
 }
